5d9a2f23c11764488b187187e47e79913aa01204
ax-IO <g.germain98@gmail.com>
2022-03-20 18:11:35 +0100

    Extraction coord. texture + Index du sommet le plus proche lors du clic

--- glwidget.cpp
+++ glwidget.cpp
@@ -1,14 +1,11 @@
 #include "glwidget.h"
 
-
 #include <QPaintEvent>
 
 GLWidget::GLWidget(QString filepath, QWidget *parent)
-    : QOpenGLWidget{parent}, fp(filepath)
-{
-    qDebug() << "Filepath = " << fp << Qt::endl;
+    : QOpenGLWidget{parent}, fp(filepath) {
+  qDebug() << "Filepath = " << fp << Qt::endl;
 }
-
 
 void GLWidget::initializeGL() {
   qDebug() << "initializeGL()" << Qt::endl;
@@ -19,6 +16,8 @@ void GLWidget::initializeGL() {
   gl_fct->glClearColor(r, g, b, 1.0f);
 
   QImage img(fp);
+  m_width = img.width();
+  m_height = img.height();
 
   img.convertTo(QImage::Format_RGBA8888);
 
@@ -32,21 +31,25 @@ void GLWidget::initializeGL() {
 }
 
 void GLWidget::paintGL() {
-//  qDebug() << "paintGL()" << Qt::endl;
+  //  qDebug() << "paintGL()" << Qt::endl;
   gl_fct->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
   Triangulation tri{12};
-  //      TriangulationOptimizer tri_opt;
-  //  tri_opt.optimize(tri, tex);
-    Renderer renderer;
-    renderer.render(tri, tex);
 
+  TriangulationOptimizer tri_opt;
+//  tri_opt.optimize(tri, tex);
+//    for (int i =0;i< tri.size() ; i++) {
+//        qDebug()<<"vertex "<< i<<" = ("<<tri.vertices()[i].x<<","<<tri.vertices()[i].y<<")"<<Qt::endl;
+//    }
+  m_tri = tri;
+
+  Renderer renderer;
+  renderer.render(tri, tex);
 }
 
 void GLWidget::resizeGL(int w, int h) {
   qDebug() << "resizeGL(" << w << "," << h << ")" << Qt::endl;
-    gl_fct->glViewport(0, 0, w, h);
+  gl_fct->glViewport(0, 0, w, h);
   //  glMatrixMode(GL_PROJECTION);
   //  glLoadIdentity();
   //  glMatrixMode(GL_MODELVIEW);
@@ -54,7 +57,7 @@ void GLWidget::resizeGL(int w, int h) {
 }
 
 void GLWidget::qColorToRGB(const QColor &C, float &r, float &g,
-                                 float &b) const {
+                           float &b) const {
   r = normalize_0_1(C.red(), RGB_MIN, RGB_MAX);
   g = normalize_0_1(C.green(), RGB_MIN, RGB_MAX);
   b = normalize_0_1(C.blue(), RGB_MIN, RGB_MAX);
@@ -64,8 +67,41 @@ float GLWidget::normalize_0_1(float val, float min, float max) const {
   return (val - min) / (max - min);
 }
 
-void GLWidget::mousePressEvent(QMouseEvent *event)
-{
-    point = event->pos();
-    qDebug()<< "(" <<point.x()<< ","<<point.y()<<")"<<Qt::endl;
+// Retourne les coordonnées textures [0, 1] à partir des coordonnées de l'image
+// et ses dimensions
+Vec2 GLWidget::coord_imageToTexture(QPoint point_image, int width, int height) {
+  Vec2 point_texture;
+  point_texture.x = (float)point_image.x() / (float)width;
+  point_texture.y = (float)point_image.y() / (float)height;
+
+  return point_texture;
+}
+
+// Distance entre deux vec2
+float distance_vec2(Vec2 a, Vec2 b) {
+  return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
+// Retourne l'indice du sommet correspondant aux coordonnées de textures
+// (distance euclidienne minimale)
+int GLWidget::coord_textureToIndex(Vec2 texturePoint, Triangulation tri) {
+  std::vector<float> distances;
+  distances.reserve(tri.size());
+
+  for (int i = 0; i < tri.size(); i++) {
+    distances.push_back(distance_vec2(texturePoint, tri.vertices()[i]));
+  }
+
+  auto it = std::min_element(std::begin(distances), std::end(distances));
+  return std::distance(std::begin(distances), it);
+}
+
+void GLWidget::mousePressEvent(QMouseEvent *event) {
+  point = event->pos();
+  Vec2 texturepoint = coord_imageToTexture(point, m_width, m_height);
+  int index = coord_textureToIndex(texturepoint, m_tri);
+  qDebug() << "(" << point.x() << "," << point.y() << ") --> ("
+           << texturepoint.x << "," << texturepoint.y
+           << ") correspond au sommet" << index << "("
+           << m_tri.vertices()[index].x << "," << m_tri.vertices()[index].y
+           << ")" << Qt::endl;
 }
--- glwidget.h
+++ glwidget.h
@@ -16,8 +16,12 @@
 #include "triangulationoptimizer.h"
 #include "renderer.h"
 
+#include "math.h"
+#include <algorithm>
+
 #define RGB_MIN 0.0f
 #define RGB_MAX 255.0f
+
 class GLWidget : public QOpenGLWidget, public QOpenGLFunctions {
   Q_OBJECT
 
@@ -33,10 +37,16 @@ protected:
 private:
   void qColorToRGB(const QColor &C, float&r, float &g, float &b) const;
   float normalize_0_1 (float val, float min, float max)const;
+  Vec2 coord_imageToTexture(QPoint point_image, int width, int height);
+  int coord_textureToIndex (Vec2 texturePoint, Triangulation tri);
 
 private:
   GLuint tex;
   QString fp ;
+
+  int m_width;
+  int m_height;
+  Triangulation m_tri;
 
   QOpenGLExtraFunctions* gl_fct;
 
